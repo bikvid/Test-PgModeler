55f1a58002394bd53cbadc4cb8abaf3d3d219bce
bikvid <shepilov_redrum@mail.ru>
2024-11-05 12:36:01 +0300

    DDL различий модели с версией базы данных

---
+++ diff.sql
@@ -0,0 +1,23 @@
+-- Diff code generated with pgModeler (PostgreSQL Database Modeler)
+-- pgModeler version: 1.1.4
+-- Diff date: 2024-11-05 12:27:19
+-- Source model: EMD_ESG
+-- Database: EMD_ESG
+-- PostgreSQL version: 15.0
+
+-- [ Diff summary ]
+-- Dropped objects: 0
+-- Created objects: 1
+-- Changed objects: 0
+
+SET search_path=public,pg_catalog,"3NF_Mod_GZT";
+-- ddl-end --
+
+
+-- [ Created objects ] --
+-- object: buffer_column | type: COLUMN --
+-- ALTER TABLE "3NF_Mod_GZT"."Object_" DROP COLUMN IF EXISTS buffer_column CASCADE;
+ALTER TABLE "3NF_Mod_GZT"."Object_" ADD COLUMN buffer_column smallint;
+-- ddl-end --
+
+
